b4f588f504da34caaca58c898ee0dda08bcc9590
Evilhenrylol <86890430+Epichenry2013@users.noreply.github.com>
2025-02-27 20:01:16 +0000

    updated to install

--- Dark Tool V2/START.bat
+++ Dark Tool V2/START.bat
@@ -1,3 +1,22 @@
 @echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+set modules=colorama requests discord rich asyncio threading json base64 subprocess shutil
+
+python --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is not installed. Please install python 3.10.9 or any 3.10.
+    pause
+    exit /b
+)
+
+for %%m in (%modules%) do (
+    python -c "import %%m" 2>nul
+    if errorlevel 1 (
+        echo Installing %%m...
+        python -m pip install %%m
+    )
+)
+
 python Main.py
 pause
